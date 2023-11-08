94b6932f1453b6c743b607010b6fc320346db8e3
Ahsan <ahsanaz461@gmail.com>
2023-11-08 10:33:19 +0700

    deps: install dotenv

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-  console.log(`App started on port ${await app.getUrl()}`);
-}
-bootstrap();
